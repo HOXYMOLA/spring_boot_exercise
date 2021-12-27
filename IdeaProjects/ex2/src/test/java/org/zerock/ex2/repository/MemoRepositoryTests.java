b10508f2acf56095ba76a26e42f62aac92fcd604
Jaeguk CHO <hoxymola@gmail.com>
2021-12-27 17:01:56 +0900

    2.5.2 페이징 처리

--- IdeaProjects/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ IdeaProjects/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,9 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entitiy.Memo;
 
@@ -69,12 +72,44 @@ public class MemoRepositoryTests {
 //        System.out.println(memoRepository.save(memo));
 //    }
 
+//    @Test
+//    public void testDelete() {
+//
+//        Long mno = 100L;
+//
+//        memoRepository.deleteById(mno);
+//
+//    }
+
     @Test
-    public void testDelete() {
+    public void testPageDefault() {
 
-        Long mno = 100L;
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
 
-        memoRepository.deleteById(mno);
+        Page<Memo> result = memoRepository.findAll(pageable);
 
+        System.out.println(result);
+
+        System.out.println("---------------------------------------");
+
+        System.out.println("Total pages: "+result.getTotalPages()); //총 몇 페이지
+
+        System.out.println("Total Count: "+result.getTotalElements()); //전체 개수
+
+        System.out.println("Page Number: "+result.getNumber()); //현재 페이지 번호
+
+        System.out.println("Page Size: "+result.getSize()); //페이지당 데이터 개수
+
+        System.out.println("has next page?: "+result.hasNext()); //다음 페이지 존재 여부
+
+        System.out.println("first page?: "+result.isFirst()); //시작 페이지(0) 여부
+
+        System.out.println("---------------------------------------");
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
     }
+
 }
