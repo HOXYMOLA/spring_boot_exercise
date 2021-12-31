d2a6ef47f05cee033193411768e7c2dacdbfab8e
Jaeguk CHO <hoxymola@gmail.com>
2022-01-01 04:40:38 +0900

    4.3 엔티티 클래스와 Querydsl 설정

---
+++ IdeaProjects/guestbook/src/main/java/org/zerock/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package org.zerock.guestbook.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.guestbook.entitiy.Guestbook;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
