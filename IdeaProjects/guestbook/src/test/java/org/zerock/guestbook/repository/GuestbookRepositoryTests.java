ee6691a70ffd07fbe162d371a0e50712ef9cccdc
Jaeguk CHO <hoxymola@gmail.com>
2022-01-01 11:45:01 +0900

    4.3.3 Querydsl 테스트

--- IdeaProjects/guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ IdeaProjects/guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entitiy.Guestbook;
+import org.zerock.guestbook.entitiy.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -28,19 +35,77 @@ public class GuestbookRepositoryTests {
 //        });
 //    }
 
+//    @Test
+//    public void updateTest(){
+//
+//        Optional<Guestbook> result = guestbookRepository.findById(300L); //존재하는 번호로 테스트
+//
+//        if(result.isPresent()){
+//
+//            Guestbook guestbook = result.get();
+//
+//            guestbook.changeTitle("Changed Title....");
+//            guestbook.changeContent("Changed Content...");
+//
+//            guestbookRepository.save(guestbook);
+//        }
+//    }
+
+//    @Test
+//    public void testQuery1(){
+//    //제목(title)에 '1'이라는 글자가 있는 엔티티들을 검색
+//
+//        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+//
+//        QGuestbook qGuestbook = QGuestbook.guestbook;
+//        //동적으로 처리하기 위해 Q도메인 클래스를 얻어온다.
+//
+//        String keyword = "1";
+//
+//        BooleanBuilder builder = new BooleanBuilder();
+//        //BooleanBuilder는 where문에 들어가는 조건들을 넣어주는 컨테이너
+//
+//        BooleanExpression expression = qGuestbook.title.contains(keyword);
+//        //원하는 조건은 필드 값과 같이 결합해서 생성
+//
+//        builder.and(expression);
+//        //만들어진 조건은 where문에 and나 or같은 키워드와 결합
+//
+//        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+//        //BooleanBuilder는 GuestbookRepository에 추가 된 QuerydslPredicateExcutor 인터페이스의 findAll() 사용 가능
+//
+//        result.stream().forEach(guestbook -> {
+//            System.out.println(guestbook);
+//        });
+//    }
+
     @Test
-    public void updateTest(){
+    public void testQuery2() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
 
-        Optional<Guestbook> result = guestbookRepository.findById(300L); //존재하는 번호로 테스트
+        QGuestbook qGuestbook = QGuestbook.guestbook;
 
-        if(result.isPresent()){
+        String keyword = "1";
 
-            Guestbook guestbook = result.get();
+        BooleanBuilder builder = new BooleanBuilder();
 
-            guestbook.changeTitle("Changed Title....");
-            guestbook.changeContent("Changed Content...");
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
 
-            guestbookRepository.save(guestbook);
-        }
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+
+        BooleanExpression exAll = exTitle.or(exContent);
+        //exTitle과 exContent를 결합
+
+        builder.and(exAll);
+        //BooleanBuilder에 추가
+
+        builder.and(qGuestbook.gno.gt(0L));
+        //'gno가 0보다 크다' 조건을 추가
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
     }
 }
